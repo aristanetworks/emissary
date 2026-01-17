25877d10dde6827b944a3a1abc3455ebea4cd75e
Connor Luckett <connor.luckett@arista.com>
2026-01-16 16:00:38 -0800

    rename setup to avoid nose conflict

--- python/kat/harness.py
+++ python/kat/harness.py
@@ -1005,7 +1005,7 @@ class Runner:
                 )
 
                 with capsys.disabled():
-                    self.setup(selected)
+                    self.do_setup(selected)
 
                 # XXX: should aggregate the result of url checks
                 i = 0
@@ -1026,7 +1026,7 @@ class Runner:
     def __call__(self):
         assert False, "this is here for py.test discovery purposes only"
 
-    def setup(self, selected):
+    def do_setup(self, selected):
         if not self.done:
             if not DOCTEST:
                 print()
